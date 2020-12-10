89a5f526c4ed27f04de16b8c07d1701f352939d6
sbrinkmeyer <sbrinkmeyer@gmail.com>
2020-12-10 15:28:48 -0600

    loopy loops

--- src/mdb.py
+++ src/mdb.py
@@ -9,9 +9,12 @@ movie_name = sys.argv[1]
 TMDB_API_KEY = os.getenv('TMDB_API_KEY')
 
 tmdb.API_KEY = TMDB_API_KEY
-search = tmdb.Search()
-response = search.movie(query=movie_name)
-for s in search.results:
-    print( s['title'] + ' (' + s['release_date'].split("-")[0] + ') [480p]\\' + s['title'] + '.' + s['release_date'].split("-")[0] + '.480p.mv4' )
 
+while movie_name != '':
+  search = tmdb.Search()
+  response = search.movie(query=movie_name)
+  for s in search.results:
+      print( s['title'] + ' (' + s['release_date'].split("-")[0] + ') [480p]\\' + s['title'] + '.' + s['release_date'].split("-")[0] + '.480p.mv4' )
+  print(' == Another search?')
+  movie_name = input()
 
